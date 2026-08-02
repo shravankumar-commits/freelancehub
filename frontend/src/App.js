2d18c9f3f1bf914a18633e373db0927dfc4bc2c7
shravankumar212-commits <Shravankumarnedhunuri212@gmail.com>
2026-08-02 11:54:37 +0530

    Initialize React frontend and create reusable Navbar component

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App(){
+  return (
+    <div> 
+      <Navbar />
+
+      <hr />
+
+      <h2>Welcome toFreelanceHub </h2>
+      <p>A platform where clients can post s project sand freelancers can bid on the projects. </p>
+    </div>
+  )
+
+}
+export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <nav>
+            <h2> FreelanceHub</h2>
+            
+            <div>
+            <button>Home</button>
+            <button>Projects</button>
+            <button>Login</button>
+            <button>Register</button>
+            </div>
+
+        </nav>
+    );
+}
+export default Navbar;
